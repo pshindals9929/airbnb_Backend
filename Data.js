f943ddfbee3eceff18d8ccf6d8646cb5646ebb8c
Prakash Shindalkar <Prakash.Shindalkar@aexp.com>
2020-10-29 21:05:14 +0530

    Init

---
+++ Data.js
@@ -0,0 +1,25 @@
+const Data = [
+  {
+    id: 1,
+    title: "Unique stays",
+    description: "places that are more than just a place to sleep",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSU-sKnQyoninzDjScpOt65nUkKbk1pg76V_A&usqp=CAU",
+  },
+  {
+    id: 2,
+    title: "Online Experiences",
+    description: "A new way to travel from home",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPAIWXvywvVcGSPIQapwr8ZV4t1NHfLl-7HQ&usqp=CAU",
+  },
+  {
+    id: 3,
+    title: "Entire Homes",
+    description: "Comfortable private places ,with room for freiends or family",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqBezMUy1tcp5GDKltOAYxV3Ab3Bnf995e5Q&usqp=CAU",
+  },
+];
+
+module.exports = Data;
